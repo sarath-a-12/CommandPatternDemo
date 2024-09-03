9530acab84df6f80592621c226fb38e9654a6c61
Sarath <Sarath@SARATH_A>
2024-09-03 23:00:22 +0530

    Add project files.

---
+++ CommandTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Command;
+namespace CommandTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DeleteTester()
+        {
+            DeleteFileCommand deleter = new("C:/temp/users/notes.txt");
+            Assert.AreEqual( "Deleting File from C:/temp/users/notes.txt" , deleter.Execute() );
+        }
+
+        [TestMethod]
+        public void UpdateTester()
+        {
+            UploadFileCommand uploader = new("C:/temp/users/notes1.txt" );
+            Assert.AreEqual( "Uploading File to C:/temp/users/notes1.txt" , uploader.Execute() );
+        }
+
+        [TestMethod]
+        public void DownloadTester()
+        {
+            DownloadFileCommand downloader = new( "C:/temp/users/notes2.txt" );
+            Assert.AreEqual( "Downloading File to C:/temp/users/notes2.txt" , downloader.Execute() );
+        }
+    }
+}
